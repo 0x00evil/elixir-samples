8e9a81c1059deb87f9782d0effd8ec7ea735ea8a
高跃华 <gaoyuehua@goyoo.com>
2015-11-10 17:27:36 +0800

    elixir中guard的用法

---
+++ guards.exs
@@ -0,0 +1,22 @@
+defmodule Guard do
+  def what_is(x) when is_number(x) do
+    IO.puts "#{x} is a number"
+  end
+
+  def what_is(x) when is_list(x) do
+    IO.puts "#{inspect(x)} is a list"
+  end
+
+  def what_is(x) when is_atom(x) do
+    IO.puts "#{x} is a atom"
+  end
+end
+
+Guard.what_is(9)
+|> IO.puts
+
+Guard.what_is([1,2,3])
+|> IO.puts
+
+Guard.what_is(:atom)
+|> IO.puts
