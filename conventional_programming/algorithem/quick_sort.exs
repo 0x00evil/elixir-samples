1e28e74f4c1e26078cacb737d9d070716d509c57
高跃华 <gaoyuehua@goyoo.com>
2015-11-17 19:02:39 +0800

    开始写算法了

---
+++ conventional_programming/algorithem/quick_sort.exs
@@ -0,0 +1,9 @@
+defmodule QuickSort do
+  def sort([]), do: []
+  def sort([head|tail]) do
+    {lesser, greater} = Enum.partition(tail, &(&1 < head))
+    sort(lesser) ++ [head] ++ sort(greater)
+  end
+end
+
+IO.inspect QuickSort.sort([1,6,3,4,2,5])
